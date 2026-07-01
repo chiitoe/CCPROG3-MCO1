3f5d7f833d0d3809126a0dc65f5a541e2958bb74
unknown <elyshaaudrey06@gmail.com>
2026-07-01 21:44:58 +0800

    character class complete

--- Espiritu_Leh_MCO1/src/Character.java
+++ Espiritu_Leh_MCO1/src/Character.java
@@ -5,7 +5,7 @@ public abstract class Character {
     private final int characterID;
     private String name;
     private String alias;
-    private String origin;
+    private final String origin;
     private Status status;  // used an enum for status to prevent typos when calling
     private String devilFruitPower;
     private int wallet;
@@ -13,12 +13,12 @@ public abstract class Character {
     // constructor
     protected Character(String name, String alias, String origin, int wallet){
         this.characterID = autoID++;        // returns value THEN increments - first character will have ID 1
-        this.name = name;
-        this.alias = alias;
-        this.origin = origin;
+        this.name = (name != null && !name.isBlank()) ? name:"Unknown";
+        this.alias = (alias != null && !alias.isBlank()) ? alias:"Unknown";
+        this.origin = (origin != null && !origin.isBlank()) ? origin:"Unknown";
         this.status = Status.FREE;
         this.devilFruitPower = null;
-        this.wallet = wallet;
+        this.wallet = (wallet >= 0) ? wallet:0;
     }
 
     public void displayProfile(){
@@ -34,7 +34,83 @@ public abstract class Character {
 
     // GETTERS
 
+    public int getCharacterID(){
+        return this.characterID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAlias(){
+        return this.alias;
+    }
+
+    public String getOrigin(){
+        return this.origin;
+    }
+
+    public Status getStatus(){
+        return this.status;
+    }
+
+    public String getDevilFruitPower(){
+        return this.devilFruitPower;
+    }
+
+    public int getWallet(){
+        return this.wallet;
+    }
+
     // SETTERS
+
+    public void setName(String newName){
+        if(newName != null && !newName.isBlank()){
+            this.name = newName;
+        }
+    }
+
+    public void setAlias(String newAlias){
+        if(newAlias != null && !newAlias.isBlank()){
+            this.alias = newAlias;
+        }
+    }
+
+    public void setStatus(Status newStatus){
+        this.status = newStatus;
+
+        if(this.status == Status.DEAD){
+            this.devilFruitPower = null;
+        }
+    }
+
+    public void setDevilFruitPower(String devilFruit){
+        if (devilFruit != null && !devilFruit.isBlank()){
+            this.devilFruitPower = devilFruit;
+        }
+    }
+
+    // wallet modifiers are divided into two for ease of use. since these actions have potential of failing due to misinput, the methods return boolean values to indicate a success or failure.
+
+    public boolean addWallet(int amount){
+        if(amount > 0) {
+            this.wallet += amount;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean deductWallet(int amount){
+        if (hasEnoughMoney(amount)){
+            this.wallet -= amount;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     // HELPER FUNCTIONS
 
