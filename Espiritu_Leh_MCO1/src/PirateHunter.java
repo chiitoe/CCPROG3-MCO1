914e165aba0f237f60bfc0888f075e9376f31c5e
Gene Clement Espiritu <gene_espiritu@dlsu.edu.ph>
2026-07-10 20:59:37 +0800

    Update PirateHunter.java

--- Espiritu_Leh_MCO1/src/PirateHunter.java
+++ Espiritu_Leh_MCO1/src/PirateHunter.java
@@ -1,10 +1,15 @@
+// Module 2 setup a character not affiliated with a Pirate Crew or Marine Corp 
 public class PirateHunter extends Character {
 
-    // child attributes
+    // Child attributes
     private String combatStyle;
     private int confirmedCaptures;
 
-    // constructor
+    /* CONSTRUCTOR
+        Purpose: Creates a pirate hunter.
+        @ combatStyle: falls back to "None" if null/blank.
+        @ confirmedCaptures: falls back to 0 if negative.
+    */
     public PirateHunter(String name, String alias, String origin, int wallet, String combatStyle, int confirmedCaptures) {
         super(name, alias, origin, wallet);
 
@@ -12,22 +17,16 @@ public class PirateHunter extends Character {
         this.confirmedCaptures = (confirmedCaptures >= 0) ? confirmedCaptures : 0;
     }
 
-    // getters
-    public String getCombatStyle(){
-        return this.combatStyle;
-    }
-    public int getConfirmedCaptures(){
-        return this.confirmedCaptures;
-    }
+    // Getters
+    public String getCombatStyle(){ return this.combatStyle; }
+    public int getConfirmedCaptures(){ return this.confirmedCaptures; }
 
-    // setters
-
+    // Setters
     public void setCombatStyle(String combatStyle){
         if (combatStyle != null && !combatStyle.isBlank()){
             this.combatStyle = combatStyle;
         }
     }
-
     public void setConfirmedCaptures(int confirmedCaptures){
         this.confirmedCaptures = (confirmedCaptures >= 0) ? confirmedCaptures:0;
     }
